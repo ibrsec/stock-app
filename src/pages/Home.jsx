0d599b851d401b823fcb7a92bd57bbf6cd3aacf5
ibr <ibr.seckin@gmail.com>
2024-05-08 22:10:18 +0300

    dashboard todos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <ul>
+        <li>TODO</li>
+        <li>
+          <input type="checkbox" name="firms" id="firms" checked/>
+          <label htmlFor="firms">Firms</label>
+        </li>
+        <li>
+          <input type="checkbox" name="firms" id="brands" checked/>
+          <label htmlFor="brands">Brands</label>
+        </li>
+        <li>
+          <input type="checkbox" name="firms" id="purchases" />
+          <label htmlFor="purchases">Purchases</label>
+        </li>
+        <li>
+          <input type="checkbox" name="firms" id="Sales" />
+          <label htmlFor="Sales">Sales</label>
+        </li>
+        <li>
+          <input type="checkbox" name="firms" id="Products" />
+          <label htmlFor="Products">Products</label>
+        </li>
+        <li>
+          <input type="checkbox" name="firms" id="Dashboard" />
+          <label htmlFor="Dashboard">Dashboard</label>
+        </li>
+      </ul>
+    </div>
   )
 }
 
