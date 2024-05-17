0d34a6ce858ac7a1504882369b1c97cd3e253b2f
ibr <ibr.seckin@gmail.com>
2024-05-17 08:23:23 +0300

    kpi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import Chart from "../components/charts/Chart";
 import { useSelector } from "react-redux";
-import { Box } from "@mui/material";
 import useStockRequest from "../services/useStockRequest";
+import { Box } from "@mui/material";
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 import BalanceIcon from '@mui/icons-material/Balance';
+import Kpi from "../components/kpi/Kpi";
 const Home = () => {
   const { purchases, sales } = useSelector((state) => state.stock);
   const {getDataApi,getAllDataGenericApi} = useStockRequest();
@@ -23,35 +24,7 @@ const Home = () => {
 
   return (
     <>
-      <Box display={"flex"} alignItems="start" justifyContent="center" gap={2} mt={5} flexWrap="wrap" >
-        <Box display={"flex"} alignItems="start" justifyContent="center" gap={3} borderRadius={2} backgroundColor="primary.main" p={2}>
-          <Box variant="span" component="span" sx={{backgroundColor:"lightpink",borderRadius:"50%",color:"purple",padding:"12px"}} display="flex" alignItems="center" justifyContent="center"><MonetizationOnIcon/></Box>
-          <Box>
-            <Box sx={{color:"whiteSpec.main",fontWeight:"600"}}>Sales</Box>
-            <Box sx={{color:"whiteSpec.main",whiteSpace:"nowrap"}}>$ {totalSales}</Box>
-          </Box>
-        </Box>
-
-
-
-        <Box display={"flex"} alignItems="start" justifyContent="center" gap={3} borderRadius={2} backgroundColor="primary.main" p={2}>
-          <Box variant="span" component="span" sx={{backgroundColor:"#D9E291",borderRadius:"50%",color:"#62304A",padding:"12px"}} display="flex" alignItems="center" justifyContent="center"><BalanceIcon/></Box>
-          <Box>
-            <Box sx={{color:"whiteSpec.main",fontWeight:"600"}}>Profit</Box>
-            <Box sx={{color:"whiteSpec.main",whiteSpace:"nowrap"}}>$ {profit}</Box>
-          </Box>
-        </Box>
-
-
-
-        <Box display={"flex"} alignItems="start" justifyContent="center" gap={3} borderRadius={2} backgroundColor="primary.main" p={2}>
-          <Box variant="span" component="span" sx={{backgroundColor:"#32CAFA",borderRadius:"50%",color:"#9F4C38",padding:"12px"}} display="flex" alignItems="center" justifyContent="center"><ShoppingCartCheckoutIcon/></Box>
-          <Box>
-            <Box sx={{color:"whiteSpec.main",fontWeight:"600"}}>Purchases</Box>
-            <Box sx={{color:"whiteSpec.main",whiteSpace:"nowrap"}}>$ {totalPurchases}</Box>
-          </Box>
-        </Box>
-      </Box>
+      <Kpi />
        
 
 
